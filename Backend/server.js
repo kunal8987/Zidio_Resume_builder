d079e5035cdd8bacf700ccde04c8b01f16c9c064
Kunal Deotale <kdeotale4@gmail.com>
2024-05-28 00:37:13 +0530

    react app and the backend folder with the server file

---
+++ Backend/server.js
@@ -0,0 +1,3 @@
+
+
+console.log('This Is The Server File Main File Of Backend');
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-lime-400 mt-10">This Is The Frontend Main File </h1>
+    </div>
+  );
+}
+
+export default App;
